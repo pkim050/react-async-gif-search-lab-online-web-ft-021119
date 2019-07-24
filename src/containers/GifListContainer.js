fb6ec34d6533518362b06976e3278959c9cf40d4
Patrick Kim <pkim050@gmail.com>
2019-07-24 18:53:36 +0000

    Automatically backed up by Learn

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import GifList from '../components/GifList'
+import GifSearch from '../components/GifSearch'
 
 class GifListContainer extends React.Component {
   state = {}

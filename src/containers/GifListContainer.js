51091d3717e1190e65e72fe3080127affb5859b7
Patrick Kim <pkim050@gmail.com>
2019-07-24 18:50:49 +0000

    Automatically backed up by Learn

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -1,7 +1,25 @@
 import React from 'react'
 
 class GifListContainer extends React.Component {
+  state = {}
 
+  componentDidMount() {
+    fetch('')
+    .then(resp => resp.json())
+    .then(json => {
+      this.setState({
+        data: json
+      })
+    })
+  }
+
+  render() {
+    return(
+      <div>
+
+      </div>
+    )
+  }
 }
 
 export default GifListContainer

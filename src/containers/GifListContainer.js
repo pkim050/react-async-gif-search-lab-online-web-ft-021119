a0e3bf029e74ea467254c09faf5f90276229d41f
Patrick Kim <pkim050@gmail.com>
2019-07-25 09:08:51 +0000

    Automatically backed up by Learn

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -3,26 +3,31 @@ import GifList from '../components/GifList'
 import GifSearch from '../components/GifSearch'
 
 class GifListContainer extends React.Component {
-  state = {}
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: null
+        }
+        this.search = React.createRef()
+    }
 
-  componentDidMount() {
-    fetch('')
-    .then(resp => resp.json())
-    .then(json => {
-      this.setState({
-        data: json
-      })
-    })
-  }
+    handleFetch = event => {
+        fetch(`http://api.giphy.com/v1/gifs/search?q=${event}&api_key=dc6zaTOxFJmzC&rating=g`)
+        .then(resp => resp.json())
+        .then(json => this.setState({
+                data: json.data.slice(0, 3)
+            })
+        )
+    }
 
-  render() {
-    return(
-      <div>
-        <GifList />
-        <GifSearch />
-      </div>
-    )
-  }
+    render() {
+        return(
+            <div>
+                <GifSearch ref={this.search} handleSubmit={this.handleFetch} />
+                <GifList gifs={this.state.data} />
+            </div>
+        )
+    }
 }
 
 export default GifListContainer

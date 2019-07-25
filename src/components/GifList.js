8a45567cd3fb0850e147ca33998053c3c1e3f962
Patrick Kim <pkim050@gmail.com>
2019-07-25 09:08:55 +0000

    Automatically backed up by Learn

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,20 +1,26 @@
 import React from 'react'
 
 class GifList extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      gif: props
+    constructor(props) {
+        super(props)
+        this.state = {
+            gifs: props
+        }
     }
-  }
 
-  render() {
-    return(
-      <ul>
-        <li>{this.state.gif}</li>
-      </ul>
-    )
-  }
+    handleGifs = () => {
+        if (this.props.gifs !== null) {
+            return this.props.gifs.map(gif => <li><img src={gif.images.original.url} alt="" /></li>)
+        }
+    }
+
+    render() {
+        return(
+            <ul>
+                {this.handleGifs()}
+            </ul>
+        )
+    }
 }
 
 export default GifList
